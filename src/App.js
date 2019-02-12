8eaddf44cf63a98cf61bc85949c9d9379c4849e6
gilian <g.denis@neolynk.com>
2019-02-12 17:20:24 +0100

    # data + container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import funcProg from './data/functionnalProgramming';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        hello world
+function App(){
+  const { title, pageList } = funcProg;
+  return (
+    <div className="App">
+      <div className="title">{title}</div>
+      <div className="container">
+        <h2>{pageList[0].title}</h2>
+        <div className="content">{pageList[0].content}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/functionnalProgramming.js
@@ -0,0 +1,35 @@
+const functionnalProg = {
+  title: 'functionnal programming',
+  pageList: [
+    {
+      title: 'immutable',
+      content: 'fisrt'
+    },
+    {
+      title: 'immutable',
+      content: 'seconde'
+    },
+    {
+      title: 'immutable',
+      content: '3'
+    },
+    {
+      title: 'fonction pure',
+      content: '4'
+    },
+    {
+      title: 'fonction pure',
+      content: '5'
+    },
+    {
+      title: 'recursive',
+      content: '4'
+    },
+    {
+      title: 'recursive',
+      content: '5'
+    },
+  ],
+}
+
+export default functionnalProg;
